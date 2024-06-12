178076db361318ee2d361dfa53f9bd209ed64e46
Shubhcyb2611 <s.gupta@cybtekk.com>
2024-06-12 10:59:01 +0530

    feat : payloads added

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import { Logger } from "./middleware/logger.js";
 import genericRouter from "../src/routers/router-Request.js";
 import { REST_PORT, GRAPHQL_PORT, GRPC_PORT } from "./config/env.config.js";
+import { requestPayload } from "./middleware/payload.js";
 
 const ports = {
   REST: REST_PORT,
@@ -9,10 +10,10 @@ const ports = {
   gRPC: GRPC_PORT,
 };
 
-
 for (const [apiType, port] of Object.entries(ports)) {
   const app = express();
   app.use(express.json());
+  app.use(requestPayload);
 
   app.use("/api", genericRouter);
 
--- src/middleware/payload.js
+++ src/middleware/payload.js
@@ -3,7 +3,7 @@ export const requestPayload = async (req, res, next) => {
   let payloadType = "";
   if (req.body) {
     payloadSize = JSON.stringify(req.body).length;
-    payloadType = req.headers["Content-type"];
+    payloadType = req.headers["content-type"];
   }
   console.log(`Payload Size: ${payloadSize} bytes`);
   console.log(`Payload Type: ${payloadType}`);
